04fd9119ca56a384133f1e545a173fbee804a972
Alexande Becker <becker.alexander42@gmail.com>
2021-06-09 10:51:46 +0200

    moved the main function to an async task and moved the plotting class into a different file

--- mbclient.py
+++ mbclient.py
@@ -1,75 +1,12 @@
 #!/usr/bin/python3
 
-# WS client example
-
-import asyncio
-from aiofile import AIOFile
-import functools
-import sys
 import argparse as ap
-import websockets
+import asyncio
 import time
 import numpy as np
-import matplotlib.pyplot as plt
+import websockets
 import mbdatatypes as mbd
-import multiprocessing as mp
-
-class ProcessPlotter:
-    def __init__(self):
-        self.data = []
-        self.patches = None
-        self.hist = None
-
-    def terminate(self):
-        plt.close('all')
-
-    def call_back(self):
-        while self.pipe.poll():
-            command = self.pipe.recv()
-            if command is None:
-                self.terminate()
-                return False
-            else:
-                h, _ = np.histogram(command, self.bins)
-                self.hist_values += h
-                for c, r in zip(self.hist_values, self.patches.patches):
-                    r.set_height(c)
-        self.fig.canvas.draw()
-        return True
-
-    def __call__(self, pipe, bins):
-        print('starting plotter...')
-        self.pipe = pipe
-        self.bins = bins
-        self.hist_values = np.zeros_like(bins[1:])
-        self.fig, self.ax = plt.subplots()
-        self.hist, self.bins, self.patches = self.ax.hist([], self.bins,
-                color = 'blue')
-        self.ax.set_ylim(0, 5000)
-        self.ax.set_xlabel(r'Pulshoehe $\propto$ Energie')
-        self.ax.set_ylabel('Ereignisanzahl')
-        self.ax.grid()
-        self.ax.set_title('Pulshoehenspektrum')
-        timer = self.fig.canvas.new_timer(interval=500)
-        timer.add_callback(self.call_back)
-        timer.start()
-        plt.show()
-
-class NBPlot:
-    def __init__(self, bins):
-        self.plot_pipe, plotter_pipe = mp.Pipe()
-        self.plotter = ProcessPlotter()
-        self.plot_process = mp.Process(target=self.plotter, args=(plotter_pipe, bins), daemon=True)
-        self.plot_process.start()
-    def plot(self, data=None, finished=False):
-        send = self.plot_pipe.send
-        if finished:
-            send(None)
-        else:
-            if data is None:
-                data = np.random.rand(100)
-            send(data)
-
+from mbplotter import NBPlot
 
 async def process_data(uri, file_aqueue, plot_aqueue):
     """Coroutine that receives the data from the server
@@ -96,21 +33,15 @@ async def process_data(uri, file_aqueue, plot_aqueue):
         while True:
             try:
                 msg = await websocket.recv()
-            except (asyncio.CancelledError, websockets.exceptions.ConnectionClosedError) as e:
-                if e is asyncio.CancelledError:
-                    await websocket.close()
+            except (asyncio.CancelledError,
+                    websockets.exceptions.ConnectionClosedError):
+                print(' '*40, end='\r')
+                print('closing websocket')
+                print('A total of {} peaks where recorded'.format(count))
                 plot_aqueue.put_nowait(None)
                 file_aqueue.put_nowait(None)
-                print('', end='\r')
-                print('Shutting Down')
-                print('Parameters of the experiment:')
-                print('k: {}\nl: {}\nm: {}'.format(args.K, args.L, args.M))
-                print('peak threshhold: {}'.format(args.peakthresh))
-                print('accumulation time: {}'.format(args.deadtime))
-                print('')
-                print('A total of {} peaks where recorded'.format(count))
                 return True
-            if not args.debug:
+            if not debug:
                 if len(msg) % 12 != 0:
                     raise ValueError("msg wrong length: {}".format(len(msg)))
                 peaks = len(msg)/12
@@ -119,28 +50,16 @@ async def process_data(uri, file_aqueue, plot_aqueue):
                     pd = msg[i*12:(i+1)*12]
                     decoded_peaks.append(mbd.MeasuredPeak.decode_from_bytes(pd))
                     count += 1
-                    print("measured peaks: {}".format(count), end='\r')
-                file_aqueue.put_nowait(decoded_peaks)
-                plot_aqueue.put_nowait(decoded_peaks)
-            else:
-                if msg is None:
-                    websocket.close()
-                    plot_aqueue.put_nowait(None)
-                    file_aqueue.put_nowait(None)
-                    print('', end='\r')
-                    print('Shutting Down')
-                    print('Parameters of the experiment:')
-                    print('k: {}\nl: {}\nm: {}'.format(args.k, args.l, args.m))
-                    print('peak threshhold: {}'.format(args.peakthresh))
-                    print('accumulation time: {}'.format(args.deadtime))
-                    print('')
-                    print('A total of {} peaks where recorded'.format(count))
-                    return True
+                for peak in peaks:
+                    file_aqueue.put_nowait(peaks)
+                    plot_aqueue.put_nowait(peaks)
+                print("measured peaks: {}".format(count), end='\r')
+            else:  # this is the debug part
                 peak = mbd.MeasuredPeak.decode_from_line(msg)
                 count += 1
                 print("measured peaks: {}".format(count), end='\r')
-                await file_aqueue.put(peak)
-                await plot_aqueue.put(peak)
+                file_aqueue.put_nowait(peak)
+                plot_aqueue.put_nowait(peak)
 
 
 async def plot_data(queue, plotter):
@@ -150,8 +69,7 @@ async def plot_data(queue, plotter):
             data = await queue.get()
         except asyncio.CancelledError:
             plotter.plot(None)
-            print('Shutting down plotter ...')
-            time.sleep(3)
+            print('Shutting down plotter (this may take a few seconds)...')
             return True
         if data is None:
             plotter.plot(None)
@@ -166,7 +84,7 @@ async def plot_data(queue, plotter):
 
 
 async def write_to_file(filename, queue):
-   with open(filename, 'w+') as f:
+    with open(filename, 'w+') as f:
         f.write("timestamp,peak_height,cycle,speed\n")
         while True:
             try:
@@ -184,50 +102,69 @@ async def write_to_file(filename, queue):
 
 async def read_stdin() -> str:
     loop = asyncio.get_running_loop()
-    return loop.run_in_executor(None, sys.stdin.readline)
+    result = await loop.run_in_executor(None, input)
+    return result
+
+
+async def main(uri, args):
+    HIST_BINS = np.linspace(0, 15000000, 3000)
+    print('To stop the data taking, please type "stop" during execution')
+    plotter = NBPlot(HIST_BINS)
+    await asyncio.sleep(5)
+    file_aqueue = asyncio.Queue()
+    plot_aqueue = asyncio.Queue()
+    loop = asyncio.get_running_loop()
+    asyncio.create_task(write_to_file(args.output, file_aqueue))
+    asyncio.create_task(plot_data(plot_aqueue, plotter))
+    asyncio.create_task(process_data(uri, file_aqueue, plot_aqueue))
+    result = ''
+    while result not in ['stop', 'quit']:
+        result = await read_stdin()
+    print('')
+    return True
 
 
 if __name__ == '__main__':
-    parser = ap.ArgumentParser(description='Client application for the Moessbauereffect\
-            experiment, connects to the server and stores the Data on the local machine')
-    parser.add_argument('-d', '--debug', help='configures the uri for debug, overrides the IP parameter', action='store_true')
-    parser.add_argument('K', help='Filter parameter that determins the steepness pf the trapezoid', type=int)
+    parser = ap.ArgumentParser(description='Client application for\
+            the Moessbauereffect experiment, connects to the server\
+            and stores the Data on the local machine')
+    parser.add_argument('K', help='Filter parameter that determins\
+            the steepness pf the trapezoid', type=int)
     parser.add_argument('L', help='The parameter of the\
-            signal filter that determins the duration of the plateau of the trapezoid', type=int)
+            signal filter that determins the duration of the plateau\
+            of the trapezoid', type=int)
     parser.add_argument('M', help='The multiplication factor\
-            that determins the decay time of the pulse that the filter responds best to', type=int)
+            that determins the decay time of the pulse that\
+            the filter responds best to', type=int)
     parser.add_argument('peakthresh', help='The minimum hight of a detected\
             peak as not to be considered noise', type=int)
-    parser.add_argument('deadtime', help='The time the filter accumulates events\
-            for to pick the highest signal as "detected Peak", sets the maximum frequency of\
-            events that the filter can effectively distinguish', type=int)
+    parser.add_argument('accumtime', help='The time the filter accumulates\
+            events for to pick the highest signal as "detected Peak",\
+            sets the maximum frequency of events that the filter can\
+            effectively distinguish', type=int)
     parser.add_argument('IP', help='IP address of the red-pitaya\
             that is connected to the experiment', type=str)
-    parser.add_argument('output', help='File to write the data to. If this is not set\
-            the program writes to stdout')
-    parser.add_argument('-p', '--Port', help='Port of the TCP connection. defaults to 8080',
-            default=8080, type=int)
-    parser.add_argument('-c', '--count', help='Sets the number of events to collect and process\
-            before stopping automatically')
+    parser.add_argument('output', help='File to write the data to. The output\
+            is a CSV file with one line per event')
+    parser.add_argument('-p', '--Port', help='Port of the TCP connection.\
+            defaults to 8080', default=8080, type=int)
 
     args = parser.parse_args()
 
-    if not args.debug:
-        uri = f'ws://{args.IP}:{args.Port}/websocket\
-?k={args.K}&l={args.L}&m={args.M}&pthresh={args.peakthresh}&t_dead={args.deadtime}'
+    debug = True
+    if not debug:
+        URI = f'ws://{args.IP}:{args.Port}/websocket\
+?k={args.K}&l={args.L}&m={args.M}&pthresh={args.peakthresh}\
+&t_dead={args.deadtime}'
     else:
-        uri = 'ws://localhost:8080'
+        URI = 'ws://localhost:8080'
 
-    HIST_BINS = np.linspace(0, 15000000, 3000)
-    plotter = NBPlot(HIST_BINS)
-    time.sleep(5)
-    file_aqueue = asyncio.Queue()
-    plot_aqueue = asyncio.Queue()
     loop = asyncio.get_event_loop()
-    t1 = loop.create_task(write_to_file(args.output, file_aqueue))
-    t2 = loop.create_task(plot_data(plot_aqueue, plotter))
-    t3 = loop.create_task(process_data(uri, file_aqueue, plot_aqueue))
-    group = asyncio.gather(t1, t2, t3, return_exceptions=True)
+    loop.run_until_complete(main(URI, args))
+    pending = asyncio.all_tasks(loop=loop)
+    for task in pending:
+        task.cancel()
+    group = asyncio.gather(*pending, return_exceptions=True)
     loop.run_until_complete(group)
-    print('run process_data to completion')
+    loop.stop()
     loop.close()
---
+++ mbplotter.py
@@ -0,0 +1,70 @@
+""" This module holds the classes that abstract plotting a histogram
+in a different process, as not to slow down the data taking"""
+import multiprocessing as mp
+import matplotlib.pyplot as plt
+import numpy as np
+
+class ProcessPlotter:
+    """ Class that holds the environment for the plotting in the different
+    process. It is accessed through the NBPlot class from the main process"""
+    def __init__(self):
+        self.data = []
+        self.patches = None
+        self.hist = None
+        self.hist_values = None
+        self.pipe = None
+        self.bins = None
+        self.fig = None
+        self.ax = None
+
+    def call_back(self):
+        while self.pipe.poll():
+            command = self.pipe.recv()
+            if command is None:
+                # the plt.close stops the matplotlib event loop
+                plt.close('all')
+                return False
+            tmp_hist, _ = np.histogram(command, self.bins)
+            self.hist_values += tmp_hist
+            for count, rect in zip(self.hist_values, self.patches.patches):
+                rect.set_height(count)
+        self.fig.canvas.draw()
+        return True
+
+    def __call__(self, pipe, bins):
+        """ This is the function that is run in the other process it is called
+        at process creation. This is the time where the plot is created.
+        When running plt.show() matplotlib starts it's own event loop and calls
+        the redraw function 'call_back' every 500 ms."""
+        print('starting plotter...')
+        self.pipe = pipe
+        self.bins = bins
+        self.hist_values = np.zeros_like(bins[1:])
+        self.fig, self.ax = plt.subplots()
+        self.hist, self.bins, self.patches = self.ax.hist([], self.bins,
+                                                          color='navy')
+        self.ax.set_ylim(0, 5000)
+        self.ax.set_xlabel(r'Pulshoehe $\propto$ Energie')
+        self.ax.set_ylabel('Ereignisanzahl')
+        self.ax.grid()
+        self.ax.set_title('Pulshoehenspektrum')
+        timer = self.fig.canvas.new_timer(interval=500)
+        timer.add_callback(self.call_back)
+        timer.start()
+        plt.show()
+
+
+class NBPlot:
+    def __init__(self, bins):
+        self.plot_pipe, plotter_pipe = mp.Pipe()
+        self.plotter = ProcessPlotter()
+        self.plot_process = mp.Process(target=self.plotter, args=(plotter_pipe, bins), daemon=True)
+        self.plot_process.start()
+    def plot(self, data=None, finished=False):
+        send = self.plot_pipe.send
+        if finished:
+            send(None)
+        else:
+            if data is None:
+                data = np.random.rand(100)
+            send(data)
